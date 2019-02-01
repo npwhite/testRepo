2cd9eaf90f44f58f273403f36078d182f46890c4
npwhite <whitefie@ualberta.ca>
2019-01-31 19:26:31 -0700

    Update code_1.py
    removed some line remotely

--- src/code_1.py
+++ src/code_1.py
@@ -5,10 +5,10 @@ def findModInverse(a, m):
     if gcd(a, m) != 1:
         return None  # No mod inverse if a & m aren't relatively prime.
     u1, u2, u3 = 1, 0, a
-    v1, v2, v3 = 0, 1, m
+    # v1, v2, v3 = 0, 1, m
     while v3 != 0:
         q = u3 // v3  # Note that // is the integer division operator.
-        v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3),v1, v2, v3
+        v1, v2 = (u1 - q * v1), (u2 - q * v2)
     return u1 % m
 
 # Cracking Codes with Python: An Introduction to Building and Breaking Ciphers

41d80681efc123199a95e6acdaa8b5ce03dbfafd
Nicholas Whitefield <whitefie@ualberta.ca>
2019-01-31 19:27:48 -0700

    changed some stuff locally

--- src/code_1.py
+++ src/code_1.py
@@ -4,7 +4,10 @@
 def findModInverse(a, m):
     if gcd(a, m) != 1:
         return None  # No mod inverse if a & m aren't relatively prime.
-    u1, u2, u3 = 1, 0, a
+    
+    u1 = 1
+    u2 = 0
+    u3 = a
     v1, v2, v3 = 0, 1, m
     while v3 != 0:
         q = u3 // v3  # Note that // is the integer division operator.

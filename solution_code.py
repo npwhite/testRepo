54986e86ff58fa4feaedfad95abbf82f1c61863a
Nicholas Whitefield <whitefie@ualberta.ca>
2019-02-18 22:44:28 -0700

    edited solution_code.py

--- solution_code.py
+++ solution_code.py
@@ -4,5 +4,13 @@ def main():
     print("fixed another bug in this issue") 
     print("another print, wow!")
 
+"""
+Takes integer a and increments by 1, returning the result
+"""
+def new_function(a):
+    a = a + 1
+    return a
+
+
 if __name__=="__main__":
     main()

c4050dd500f828da4f6f400215b9d30f0f4e119d
Nicholas Whitefield <whitefie@ualberta.ca>
2019-02-18 17:39:14 -0700

    generic commit

--- solution_code.py
+++ solution_code.py
@@ -1,6 +1,7 @@
 def main():
     print("This is an example of a solution program")
     print("fixed bug in solution.py")    
+    print("fixed another bug in this issue") 
 
 if __name__=="__main__":
     main()
